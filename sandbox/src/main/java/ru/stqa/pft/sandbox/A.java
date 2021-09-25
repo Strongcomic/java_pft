ee67000a2f12a4b189545e2773bf8ed6b89c2a6f
Artyom Melnikov <strongcomic15@gmail.com>
2021-09-25 20:11:24 +0300

    Added some methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/A.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/A.java
@@ -2,8 +2,24 @@ package ru.stqa.pft.sandbox;
 
 class A
 {
-	public static void main(String arg[])
-	{
-		System.out.println("Hello World!");
+	public static void main(String arg[]) {
+		hello("Misha");
+		double l = 8;
+		System.out.println("Площать квадрата со стороной: " + l + " = " + area(l));
+		double a = 5;
+		double b = 4;
+		System.out.println("Площать прямоугольника со сторонами: " + a + " и " + b + " = " + area(a,b));
+	}
+	public static void hello(String somebody) {
+
+		System.out.println("Hello " + somebody);
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
