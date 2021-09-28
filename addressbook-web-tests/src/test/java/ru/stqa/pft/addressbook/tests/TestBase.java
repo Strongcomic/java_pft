9b640e3aa45587a19601d414d427e438eb006af4
Artyom Melnikov <strongcomic15@gmail.com>
2021-09-28 22:50:36 +0300

    Implemented Cross-browser compatibility
    Firefox
    Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,16 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
     app.init();
   }
 
