ff46f47a2b4bdeeda278a15d7fa52553b8afe0ab
Artyom Melnikov <strongcomic15@gmail.com>
2021-09-30 21:32:55 +0300

    Created ability to compare the size of List<WebElements>

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
